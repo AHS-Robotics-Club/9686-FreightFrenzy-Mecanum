84c47b599eb28b99ec2be92938e7d4cda4a49614
Vignesh <vigsk17@gmail.com>
2021-11-23 22:31:16 -0500

    Created basic code for bot w/ ducky spinner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DuckySpinnerCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DuckySpinnerCommand.java
@@ -2,23 +2,28 @@ package org.firstinspires.ftc.teamcode.commands;
 
 import com.arcrobotics.ftclib.command.CommandBase;
 
-import org.firstinspires.ftc.teamcode.drive.DuckySpinnerSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.DuckySpinnerSubsystem;
 
 import java.util.function.DoubleSupplier;
 
 public class DuckySpinnerCommand extends CommandBase {
     DuckySpinnerSubsystem sDuckySpinner;
-    DoubleSupplier dPower;
+    double dPower;
 
-    public DuckySpinnerCommand(DuckySpinnerSubsystem duckySpinnerSubsystem, DoubleSupplier powerSupplier) {
+    public DuckySpinnerCommand(DuckySpinnerSubsystem duckySpinnerSubsystem, double power) {
         sDuckySpinner = duckySpinnerSubsystem;
-        dPower = powerSupplier;
+        dPower = power;
         addRequirements(duckySpinnerSubsystem);
     }
 
     @Override
     public void execute() {
-        sDuckySpinner.start(dPower.getAsDouble());
+        sDuckySpinner.run(dPower);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        sDuckySpinner.stop();
     }
 
 }
